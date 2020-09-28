93732ffdc452d040d5155fafce3b86cce2312d52
Pedro Miranda <pbragamiranda@gmail.com>
2020-09-28 17:10:37 -0300

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../../../vendor/assets/dist/js/dsgov")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -24,6 +25,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
